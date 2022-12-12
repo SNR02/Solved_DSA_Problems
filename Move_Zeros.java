7809909957d4214610178580805f0c67e4519986
Shaik Nayyar Rabbani <109890201+SNR02@users.noreply.github.com>
2022-12-12 21:03:19 +0530

    Create Move_Zeros.java

---
+++ Move_Zeros.java
@@ -0,0 +1,21 @@
+// Problem Link : https://leetcode.com/problems/move-zeroes/description/
+
+// Simplest but not optimal (9 ms)
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int c=0,j=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]!=0){
+                nums[j]=nums[i];
+                j++;
+            }else{
+                c++;
+            }
+        }
+        for(int i=nums.length-c;i<nums.length;i++){
+            nums[i]=0;
+        }
+    }
+}
